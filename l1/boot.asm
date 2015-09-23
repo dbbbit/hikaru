156f29aef9de4f8eeda799296fc1801ee22afcc8
dbbbit <dbbbit@gmail.com>
2015-09-24 00:04:58 +0800

    汇编循环写法

--- l1/boot.asm
+++ l1/boot.asm
@@ -1,7 +1,15 @@
-        mov ax, 0x1     ;decimal
-        mov cx, 0x2     ;decimal
-hang: 
-        jmp hang           
+set_eax_0:  
+    mov eax, 0x0
 
+this_is_loop:
+    add eax, 0x1
+    cmp eax, 0x9
+    je end
+    jmp this_is_loop
+
+end: 
+    jmp end
+    
 times   510-($-$$)  db  0 
-dw  0xaa55
+db 0x55
+db 0xaa
