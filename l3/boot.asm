0be890d1f6b20ee23f880ff91aef7ae10a756622
dbbbit <dbbbit@gmail.com>
2015-09-25 11:54:18 +0800

    Hello, World...

--- l3/boot.asm
+++ l3/boot.asm
@@ -1,13 +1,30 @@
 [bits 16]
-mov ax, 0xB800
-mov es, ax
-mov di, 0
-mov al, '$'
-mov [es:di], al
 
-mov al, 0x70
-mov [es:di+1], al
+define_the_consts:
+    p_buf equ 0xb800
+    msg db 'Hello, World...'
+    len equ $-msg
 
-jmp $
+init_target_addr:
+    mov ax, p_buf
+    mov es, ax
+    mov di, 0x0
+
+init_source_addr:
+    mov ax, 0x07c0
+    mov ds, ax
+    mov si, 0
+
+print_loop: 
+    mov al, [ds:si] ;0x7c0 * 4 + 0 = 0x7c00
+    mov [es:di], al
+    inc si
+    add di, 2
+    cmp si, len
+    jl print_loop
+
+end:
+    jmp end
+
 times   510-($-$$)  db  0 
 dw  0xaa55
