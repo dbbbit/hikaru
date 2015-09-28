4154c5902c02350b84c7023d40f1e8d0a26908f1
dbbbit <dbbbit@gmail.com>
2015-09-28 10:14:14 +0800

    refactor l1

--- l1/boot.asm
+++ l1/boot.asm
@@ -1,15 +1,13 @@
-set_eax_0:  
-    mov eax, 0x0
+[org 0x7c00] ;mark start memory addr
 
-this_is_loop:
-    add eax, 0x1
-    cmp eax, 0x9
-    je end
-    jmp this_is_loop
+msg db 'hi'
+mov eax, [msg]
 
 end: 
     jmp end
     
 times   510-($-$$)  db  0 
+
+; end of boot mark
 db 0x55
 db 0xaa
