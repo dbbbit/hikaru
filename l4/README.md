3356b2be8200fda077e870e0a95b0612b7be4711
dbbbit <dbbbit@gmail.com>
2015-09-30 16:07:40 +0800

    keyboard interrupt

--- l4/README.md
+++ l4/README.md
@@ -2,8 +2,10 @@
 
 - A & B
 
+    press any keyboard button to get `B`
 
 ### 参考
 
 - [Real Mode](http://wiki.osdev.org/Real_mode)
 - [Interrupt Vector Table](http://wiki.osdev.org/Interrupt_Vector_Table)
+- [Interrupt_request](https://en.wikipedia.org/wiki/Interrupt_request_(PC_architecture))
--- l4/boot.asm
+++ l4/boot.asm
@@ -1,48 +1,55 @@
 [bits 16]
+PIC1_COMMAND equ 0x20
+PIC_EOI equ 0x20
 CODE_SEGMENT equ 0x07c0
 VIDEO_MEMORY equ 0xB800
-TIMER_IVT_OFFSET equ 0x0020
-GLOBAL_VAR equ 0x0
+IVT_OFFSET equ 0x0020
 
 init_handler:
-    ; handler's offset
+    ; handler's offset & segment addr
     mov ax, ir0_handler
-    mov [TIMER_IVT_OFFSET], ax 
-    ; handler's segment addr
+    mov [IVT_OFFSET], ax 
     mov ax, CODE_SEGMENT 
-    mov [TIMER_IVT_OFFSET+2], ax 
+    mov [IVT_OFFSET+2], ax 
 
-    mov ax, 0x0000
-    mov [GLOBAL_VAR], ax
-    mov di, 0
+    mov ax, ir1_handler
+    mov [IVT_OFFSET+4], ax 
+    mov ax, CODE_SEGMENT 
+    mov [IVT_OFFSET+6], ax 
+
+    ;init vidio buffer addr
+    mov ax, VIDEO_MEMORY
+    mov es, ax
 
 jmp $
 
 ir0_handler:
-    mov ax, [GLOBAL_VAR]
-    cmp ax, 0x0001
-    je B
-    A:  
-        mov cl, 'A'
-        yeild_to_b_next_time:
-            mov ax, 0x0001
-            mov [GLOBAL_VAR], ax
-        jmp print
-    B:  
-        mov cl, 'B'
-        yeild_to_a_next_time:
-            mov ax, 0x0000
-            mov [GLOBAL_VAR], ax
-    print:
-        mov ax, VIDEO_MEMORY
-        mov es, ax
-        mov [es:di], cl
-        add di, 2
-
-    send_end_signal:    
-        mov al, 0x20
-        out 0x20, al
+    ;print 'B'
+    push eax
+    mov al, 'A'
+    mov [es:di], al
+    add di, 2
+    
+    ;send end signal
+    mov al, PIC_EOI
+    out PIC1_COMMAND, al
+    pop eax
     iret
 
+
+; keyboard interrupt
+ir1_handler:
+    push eax
+    in al, 0x60,
+    mov al, 'B'
+    mov [es:di], al
+    add di, 2
+    ;send end signal
+    mov al, PIC_EOI
+    out PIC1_COMMAND, al
+    pop eax
+    iret
+
+   
 times   510-($-$$)  db  0 
 dw  0xaa55
