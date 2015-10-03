5ac8d91257a03bfb505e7b0e4692032bfc2787f6
dbbbit <dbbbit@gmail.com>
2015-10-03 20:49:07 +0800

    use byte string instead char array

--- l6/main.rs
+++ l6/main.rs
@@ -12,14 +12,14 @@ fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
 }
 
 fn main() {
-    let greeting = [' ', 'H', 'I', 'K', 'A', 'R', 'U', ':', ')'];
+    let greeting = b"> HIKARU RUNNING...";
     let color_code: u16 = 0x0400;
     for i in 0..(80 * 25) {
         unsafe {
             *((0xb8000 + i * 2) as *mut u16) = color_code;
         }
     }
-    for idx in 0..9 {
+    for idx in 0..19 {
         unsafe {
             *((0xb8000 + idx * 2) as *mut u16) = greeting[idx] as u16 + color_code;
         }
