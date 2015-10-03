40e43071c9752a7b3ea7dd7b3f5a4f8818deea4b
dbbbit <dbbbit@gmail.com>
2015-10-03 16:48:08 +0800

    test output text

--- l6/main.rs
+++ l6/main.rs
@@ -11,15 +11,17 @@ fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
     0
 }
 
-enum Color {
-    Blue       = 1,
-    Green      = 2
-}
-
 fn main() {
+    let greeting = [' ', 'H', 'I', 'K', 'A', 'R', 'U', ':', ')'];
+    let color_code: u16 = 0x0400;
     for i in 0..(80 * 25) {
         unsafe {
-            *((0xb8000 + i * 2) as *mut u16) = (Color::Blue as u16) << 12;
+            *((0xb8000 + i * 2) as *mut u16) = color_code;
+        }
+    }
+    for idx in 0..9 {
+        unsafe {
+            *((0xb8000 + idx * 2) as *mut u16) = greeting[idx] as u16 + color_code;
         }
     }
 }
