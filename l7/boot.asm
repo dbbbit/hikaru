449889f96a2cdc8a50b1d758a8ceb9ee7d73f55d
dbbbit <dbbbit@gmail.com>
2015-10-07 10:25:52 +0800

    why code segment writeable?

--- l7/boot.asm
+++ l7/boot.asm
@@ -13,15 +13,12 @@ jmp 0x08:p_mode
 
 [bits 32]
 p_mode:
-    offset equ 0x100000
-    mov eax, 0x10
-    mov ds, eax
-    mov word [ds:offset], 0xabcd
+    offset equ 0x0
+    mov dword [cs:offset], 0x12345678
 
     jmp $
 
 
-align 4
 gdt:
     ; null entry
     dq 0
@@ -29,16 +26,16 @@ gdt:
     dw 0xffff       ; limit 0:15
     dw 0x0000       ; base 0:15
     db 0x00         ; base 16:23
-    db 0b10011010   ; access byte - code
-    db 0x4f         ; flags/(limit 16:19). flag is set to 32 bit protected mode
+    db 0b10011010   ; access byte - code    P|DPL|1|1|C|R|A
+    db 0b01001111   ; flags/(limit 16:19).  G|B|0|AVL|limit[16..19]
     db 0x00         ; base 24:31
     ; data entry
-    dw 0xffff       ; limit 0:15
-    dw 0x0000       ; base 0:15
-    db 0x00         ; base 16:23
-    db 0b10010010   ; access byte - data
-    db 0x4f         ; flags/(limit 16:19). flag is set to 32 bit protected mode
-    db 0x00         ; base 24:31
+    ;dw 0xffff       ; limit 0:15
+    ;dw 0x0000       ; base 0:15
+    ;db 0x00         ; base 16:23
+    ;db 0b10010100   ; access byte - data    P|DPL|1|0|E|W|A
+    ;db 0b01001111   ; flags/(limit 16:19). G|D|0|AVL
+    ;db 0x00         ; base 24:31
 
 gdtdesc:
     dw gdtdesc - gdt    ;size
