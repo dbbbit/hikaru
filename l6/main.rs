8e6d1d5de5b8d0a8157a2ed9fe6a97b83a124ec8
dbbbit <dbbbit@gmail.com>
2015-10-25 19:47:02 +0800

    more inline asm

--- l6/main.rs
+++ l6/main.rs
@@ -12,6 +12,16 @@ fn start(_main: *const u8, _argc: isize, _argv: *const *const u8) -> isize {
 }
 
 fn main() {
+    let res: i32;
+    res = add(4, 2);
+    if res != 6 {
+        stop();
+    }
+    welcome();
+    stop();
+}
+
+fn welcome() {
     let greeting = b"> HIKARU RUNNING...";
     let color_code: u16 = 0x0400;
     for i in 0..(80 * 25) {
@@ -25,12 +35,22 @@ fn main() {
             *((0xb8000 + idx * 2) as *mut u16) = greeting[idx] as u16 + color_code;
         }
     }
+}
 
+fn add(a: i32, b: i32) -> i32 {
+    let mut c = 0;
     unsafe {
-        asm!("cli;
-              dead:\n
-                hlt;
-                jmp dead;"
-        )
+        asm!(
+            "add $2, $0"
+            :"=r"(c)
+            :"0"(a), "r"(b)
+        );
+    }
+    c
+}
+
+fn stop() {
+    unsafe {
+        asm!("hlt")
     }
 }
